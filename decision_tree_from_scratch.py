3b6edbe94649a0111dc7b92f26102be0e8806627
MatthewYacovone <mryacovo@gmail.com>
2025-03-28 10:57:48 -0400

    finished from-scratch implementation

--- decision_tree_from_scratch.py
+++ decision_tree_from_scratch.py
@@ -20,30 +20,43 @@ def split_node(X, y, index, value):
     return left, right
 
 def get_best_split(X, y, criterion):
+    """
+    Iteratively tries every possible split (feature-value combination), finds the one that creates the least impurity.
+    Returns the best split along with the data subsets.
+    """
     best_index, best_value, best_score, children = None, None, 1, None
-    for index in range(len[X[0]]):
-        for value in np.sort(np.unique(X[:, index])):
-            groups = split_node(X, y, index, value)
-            impurity = weighted_impurity([groups[0][1], groups[1][1]], criterion)
+    for index in range(len(X[0])): # iterate over each feature column in dataset X
+        for value in np.sort(np.unique(X[:, index])): # ensures I consider every possible meaningful split
+            groups = split_node(X, y, index, value) # groups hold two subsets (left and right children), each with their corresponding features and labels
+            impurity = weighted_impurity([groups[0][1], groups[1][1]], criterion) # uses the labels (y) from each subset
             if impurity < best_score:
                 best_index, best_value, best_score, children = index, value, impurity, groups
     
     return {'index': best_index, 'value': best_value, 'children': children}
 
 def get_leaf(labels):
+    """
+    Takes labels from a node and determines the most common (majority) class
+        - np.bincount(labels) counts how many times each class appears in labels
+        - .argmax() selects the index (class label) of the most frequent count
+        - Returns the most frequent class in the current node
+    Returns 
+    """
     # Obtain the leaf as the majority of the labels
     return np.bincount(labels).argmax()
 
 def split(node, max_depth, min_size, depth, criterion):
     """
-    Recursive function that links all of them together.
+    Repeatedly splits nodes recursively until there's no data left in a node or, it reaches the max allowed depth or, nodes become too small.
         - assigns a leaf node if one of two child nodes is empty
         - assigns a leaf node if the current branch depth exceeds the maximum depth allowed
         - assigns a leaf node if the node does not contain sufficient samples required for a further split
         - otherwise, it proceeds with a further split with the optimal splitting point
     """
-    left, right = node['children']
-    del (node['children'])
+    left, right = node['children'] # extracts left and right subsets
+    del (node['children']) # delete node from memory
+    # If a split results in one child being empty, the other non-empty child becomes a leaf.
+	# This prevents useless further splitting because no meaningful data partitioning occurred.
     if left[1].size == 0:
         node['right'] = get_leaf(right[1])
         return
@@ -84,3 +97,48 @@ def split(node, max_depth, min_size, depth, criterion):
         else:
             node['right'] = result
             split(node['right'], max_depth, min_size, depth+1, criterion)
+
+
+def train_tree(X_train, y_train, max_depth, min_size, criterion='gini'):
+    X = np.array(X_train)
+    y = np.array(y_train)
+    root = get_best_split(X, y, criterion)
+    split(root, max_depth, min_size, 1, criterion)
+
+    return root
+
+# Test the decision tree
+X_train = [['tech', 'professional'],
+           ['fashion', 'student'],
+           ['fashion', 'professional'],
+           ['sports', 'student'],
+           ['tech', 'student'],
+           ['tech', 'retired'],
+           ['sports', 'professional']]
+y_train = [1, 0, 0, 0, 1, 0, 1]
+tree = train_tree(X_train, y_train, 2, 2)
+
+# Displaying the tree
+
+CONDITION = {'numerical': {'yes': '>=', 'no': '<'},
+             'categorical': {'yes': 'is', 'no': 'is not'}}
+
+def visualize_tree(node, depth=0):
+    if isinstance(node, dict):
+        if node['value'].dtype.kind in ['i', 'f']:
+            condition = CONDITION['numerical']
+        else:
+            condition = CONDITION['categorical']
+        print('{}|- X{} {} {}'.format(depth * ' ', node['index'] + 1, condition['no'], node['value']))
+
+        if 'left' in node:
+            visualize_tree(node['left'], depth+1)
+        print('{}|- X{} {} {}'.format(depth * ' ', node['index'] + 1, condition['yes'], node['value']))
+
+        if 'right' in node:
+            visualize_tree(node['right'], depth+1)
+    else:
+        print(f"{depth * ' '}[{node}]")
+    
+visualize_tree(tree)
+
